2219871689e948a94db908e594ee1f27b27e8c17
heymeck <hemantgupta1408@gmail.com>
2024-09-18 15:50:00 +0530

    Learnt about the get and post method, used postman to test api created by me in file Express js, also learnt about mongoose driver which help to connect the mongodb database to nodejs.

--- Node js Day 3/Expres.js
+++ Node js Day 3/Expres.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+// get method is used to show data to frontend, Data which is already stored in database or server.
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Welcome to my Hotel. How can I help you?')
 })
 
 app.get('/chicken',(req,res)=>{
@@ -10,7 +12,19 @@ app.get('/chicken',(req,res)=>{
 })
 
 app.get('/idli',(req,res)=>{
-  res.send('regret to inform sir we do not serve idli.')
+  let idli={
+    name:"Rava idli",
+    chutney:true,
+    sambhar:true,
+    wada:false
+  }
+  res.send(idli)
+})
+
+// Post method is used to take data from frontend or in the form of a form to save the details in the database.
+
+app.post('/item',(req,res)=>{
+  res.send("Data is saved")
 })
 
 app.listen(3000,()=>{
